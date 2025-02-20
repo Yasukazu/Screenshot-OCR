31c5beb90f5c444069d0ab201e1b05089b0331bc
Yasukazu akino <yasukazu@makyno.jp>
2025-02-20 14:55:32 +0900

    get pdf image find_edges filter

--- get_image_outline.py
+++ get_image_outline.py
@@ -3,11 +3,12 @@ from path_feeder import get_last_month_path
 from pdf2image import convert_from_path
 
 img_name = "2025-01-4p8.pdf"
-imag_fullmath = get_last_month_path / img_name
-im = Image.open('star.png').convert('L')
+img_fullpath = get_last_month_path() / img_name
+imgs = convert_from_path(img_fullpath)
+
+im = imgs[0] # Image.open(img_fullpath).convert('L')
 
 # Detect edges and save
 edges = im.filter(ImageFilter.FIND_EDGES)
-edges.save('DEBUG-edges.png')
-
-from path_feeder import get_last_month_path
+out_name = img_name + '-1-edges.png'
+edges.save(out_name)
