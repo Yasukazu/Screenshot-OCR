b9620ee32b400db1cf80dd3f31bd549736928769
Yasukazu akino <yasukazu@makyno.jp>
2025-03-04 14:36:15 +0900

    BasicDigitStrokes.strokes

--- num_strokes.py
+++ num_strokes.py
@@ -2,11 +2,11 @@ from typing import Callable, Sequence
 from types import MappingProxyType
 from functools import lru_cache
 from numpy.typing import NDArray
-from strok7 import SEG_POINTS, SEGPATH_SLANT, get_segpath_for_c, Sp0, StrokeSlant, i_i_tpl
+from strok7 import SEG_POINTS, SEGPATH_SLANT, get_segpath_for_c, Sp, StrokeSlant, i_i_tpl, SegPoint
 from seg_7_digits import Seg7, SEG7_ARRAY, seg_7_array, homo_seg_7_array
 from seven_seg import SEVEN_SEG_SIZE
 
-SEG7_TO_POINTS: dict[Seg7, tuple[Sp0, Sp0]] = MappingProxyType({
+SEG7_TO_POINTS: dict[Seg7, tuple[Sp, Sp]] = MappingProxyType({
 	Seg7.A: (SEG_POINTS[0], SEG_POINTS[1]),
 	Seg7.B: (SEG_POINTS[1], SEG_POINTS[2]),
 	Seg7.C: (SEG_POINTS[2], SEG_POINTS[3]),
@@ -18,7 +18,7 @@ SEG7_TO_POINTS: dict[Seg7, tuple[Sp0, Sp0]] = MappingProxyType({
 
 NUMSTROKE_SLANT = SEGPATH_SLANT
 
-def get_segpoints_for_n(n: int)-> Sequence[tuple[Sp0, Sp0]]:
+def get_segpoints_for_n(n: int)-> Sequence[tuple[Sp, Sp]]:
 	seg7s: Sequence[Seg7] = SEG7_ARRAY[n]
 	return (SEG7_TO_POINTS[sp] for sp in seg7s)
 
@@ -27,42 +27,60 @@ SEGPOINTS_MAX = len(SEG7_ARRAY)
 def get_segpoints_max()-> int:
 	return len(SEG7_ARRAY)
 
-def get_all_segpoints()-> list[Sequence[tuple[Sp0, Sp0]]]:
+def get_all_segpoints_len()-> int:
+	return len(SEG7_ARRAY)
+
+def get_all_segpoints()-> list[Sequence[tuple[Sp, Sp]]]:
 	lst = []
 	for n in range(len(SEG7_ARRAY)):
 		lst.append(get_segpoints_for_n(n))
 	return lst
 
-def get_segpath_for_n(n: int, segpath_list = [None] * len(seg_7_array))-> Sequence[tuple[Sp0, Sp0]]:
+def set_all_segpoints(lst: list[Sequence[tuple[Sp, Sp]]]):
+	for n in range(len(SEG7_ARRAY)):
+		lst[n] = get_segpoints_for_n(n)
+
+def get_segpath_for_n(n: int, segpath_list = [None] * len(seg_7_array))-> Sequence[tuple[Sp, Sp]]:
 	for segs in seg_7_array[n]:
 		spth_list = []
 		for c in segs:
 			if c:
-				path_pair: tuple[Sp0,Sp0] = get_segpath_for_c(c).path
+				path_pair: tuple[Sp,Sp] = get_segpath_for_c(c).path
 				spth_list.append(path_pair)
 		segpath_list[n] = spth_list
 	return segpath_list	
 
-def get_segpath_list(segpath_list = [None] * len(seg_7_array))-> Sequence[Sequence[tuple[Sp0, Sp0]]]:
+def get_segpath_list(segpath_list = [None] * len(seg_7_array))-> Sequence[Sequence[tuple[Sp, Sp]]]:
 	for i, segs in enumerate(seg_7_array):
 		spth_list = []
 		for c in segs:
 			if c:
-				path_pair: tuple[Sp0,Sp0] = get_segpath_for_c(c).path
+				path_pair: tuple[Sp,Sp] = get_segpath_for_c(c).path
 				spth_list.append(path_pair)
 		segpath_list[i] = spth_list
 	return segpath_list	
 
+from seg_7_digits import SEG_POINT_DIGIT_ARRAY
+class BasicDigitStrokes:
+	f'''strokes[{SEVEN_SEG_SIZE}]: slanted strokes]'''
+
+	def __init__(self):#, _stroke_list = [None] * get_all_segpoints_len()):
+		pass
+		#set_all_segpoints(_stroke_list) self._stroke_list = _stroke_list
+
+	@classmethod
+	def strokes(cls, n: int)-> Sequence[tuple[i_i_tpl, i_i_tpl]]:
+		return [sp.value for sp in SEG_POINT_DIGIT_ARRAY[n]]
+
 class DigitStrokes:
 	f'''strokes[{SEVEN_SEG_SIZE}]: slanted strokes]'''
-	#_segpath_list: list[tuple[Sp0, Sp0]] = get_segpath_list()
 	def __init__(self, cache_factor: int=1): #, max_cache=SEVEN_SEG_SIZE):
 		self.strokes: Callable[[int, StrokeSlant, int, tuple[int, int]], Sequence[tuple[i_i_tpl, i_i_tpl]]] = lru_cache(maxsize=SEGPOINTS_MAX * cache_factor)(self._strokes)
 
-	def _strokes(self, n: int, slant: StrokeSlant=StrokeSlant.SLANT02, scale: int=1, offset: tuple[int, int]=(0, 0))-> Sequence[tuple[i_i_tpl, i_i_tpl]]:
+	def _strokes(self, n: int, slant: StrokeSlant=StrokeSlant.SLANT00, scale: int=1, offset: tuple[int, int]=(0, 0))-> Sequence[tuple[i_i_tpl, i_i_tpl]]:
 		stroke_list = []
-		for pp in get_segpoints_for_n(n): # self._segpath_list[n]:
-			p_list = [p.scale_offset(slant=slant, scale=scale, offset=offset) for p in pp] # p_list = ((scale * p.slant(slant) + offset[i]) for i, p in enumerate(pp))
+		for pp in get_segpoints_for_n(n):
+			p_list = [p.scale_offset(slant=slant, scale=scale, offset=offset) for p in pp]
 			stroke_list.append(p_list)
 		return stroke_list
 
@@ -83,10 +101,8 @@ class NumStrokes(DigitStrokes):
 
 if __name__ == '__main__':
 	from pprint import pp
-	import cProfile
 	offset=(7,3)
 	scale=2
-
 	all_strokes_list = get_all_segpoints()
 	num_strokes = DigitStrokes()
 	segpoints_range = get_segpoints_max()
