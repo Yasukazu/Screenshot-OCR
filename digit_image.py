93c29ca17f4be7851fc8f899ac55e2600ba6b23a
Yasukazu akino <yasukazu@makyno.jp>
2025-03-12 14:24:20 +0900

    BasicDigitImageParam calculates self.width in post_init

--- digit_image.py
+++ digit_image.py
@@ -63,16 +63,24 @@ class DigitImage:
 
 import digit_strokes
 from collections import namedtuple
+from dataclasses import dataclass, field
+@dataclass
+class BasicDigitImageParam:
+	scale: int
+	padding: tuple[int, int]
+	line_width: int
+	width: int = field(init=False)
+	def __post_init__(self):
+		if min(self.padding) < 0:
+			raise ValueError("Every padding value must be larger than 0!")
+		if self.line_width < 0:
+			raise ValueError("The value of line_width must be larger than 0!")
+		if self.scale - self.line_width <= 0:
+			raise ValueError("The value of scale must be larger than line_width!")
+		self.width = self.scale + self.padding[0] * 2 + self.line_width
 
-
-BasicDigitImageParam = namedtuple('BasicDigitImageParam', ['scale', 'padding', 'line_width'])
-
-BASIC_DIGIT_IMAGE_PARAM_LIST = [
-	BasicDigitImageParam(scale=8, padding=(2, 2), line_width=2),
-	BasicDigitImageParam(scale=16, padding=(4, 4), line_width=4),
-]
 class BasicDigitImage:
-	WIDTH = 8
+	WIDTH = 10
 	HEIGHT = WIDTH * 2
 	SIZE = [WIDTH, HEIGHT]
 	PADDING = (2, 2)
@@ -85,13 +93,14 @@ class BasicDigitImage:
 		return scale
 
 	@classmethod
-	def calc_scale_from_width(cls, width: int=WIDTH, padding: int=-1, line_width: int=-1)-> BasicDigitImageParam:
+	def calc_scale_from_height(cls, height: int=HEIGHT, padding: int=-1, line_width: int=-1)-> BasicDigitImageParam:
 		if padding < 0:
-			padding = width // 4
+			padding = height // 8
 		if line_width < 0:
-			line_width = width // 8 or 1
-		offset = padding + line_width // 2 or 1
-		scale = width - 2 * offset
+			line_width = height // 16 or 1
+		scale = (height - 2 * padding - line_width) // 2 
+		if scale - line_width <= 0:
+			raise ValueError(f"scale({scale}) is too small to show!")
 		return BasicDigitImageParam(scale=scale, padding=(padding, padding), line_width=line_width)
 
 	@classmethod
@@ -106,15 +115,15 @@ class BasicDigitImage:
 		size = scale, scale * 2
 		return [sz + 2 * stroke_offset[i] for i, sz in enumerate(size)]
 
-	def __init__(self, width: int, scale: int, padding: Sequence[int], line_width: int,
+	def __init__(self, param: BasicDigitImageParam,
 			slant: StrokeSlant=StrokeSlant.SLANT00, 
-			bgcolor=ImageFill.WHITE):
-		stroke_scale = scale # - padding[0] * 2 - line_width
-		stroke_offset = [pad + line_width // 2 or 1 for pad in padding]
+			bgcolor=ImageFill.WHITE): # width: int, scale: int, padding: Sequence[int], line_width: int
+		stroke_scale = param.scale # - padding[0] * 2 - line_width
+		stroke_offset = [pad + param.line_width // 2 or 1 for pad in param.padding]
 		self.stroke_feeder = digit_strokes.DigitStrokes(slant=slant, scale=stroke_scale, offset=(stroke_offset[0], stroke_offset[1]))
-		self.line_width = line_width
+		self.line_width = param.line_width
 		self.bgcolor = bgcolor
-		self.size = width, width * 2
+		self.size = param.width, param.width * 2
 		self.get: Callable[[int], Image.Image] = lru_cache(maxsize=self.stroke_feeder.get_max())(self._get)
 
 	def _get(self, n: int)-> Image.Image:
@@ -127,7 +136,7 @@ class BasicDigitImage:
 			drw.line(stroke, width=self.line_width, fill=ImageFill.invert(self.bgcolor).value, joint='curve')
 		return img
 
-def get_basic_number_image(nn: Sequence[int | FormatNum] | bytearray, digit_image_feeder=BasicDigitImage(BasicDigitImage.WIDTH, *BasicDigitImage.calc_scale_from_width()))-> Image.Image:
+def get_basic_number_image(nn: Sequence[int | FormatNum] | bytearray, digit_image_feeder=BasicDigitImage(param=BasicDigitImage.calc_scale_from_height(BasicDigitImage.HEIGHT)))-> Image.Image: #BasicDigitImage.WIDTH, 
 	b_array = nn if isinstance(nn, bytearray) else formatnums_to_bytearray(nn)
 	number_image_size = len(b_array) * digit_image_feeder.size[0], digit_image_feeder.size[1]
 	number_image = Image.new('L', number_image_size, (0,))
@@ -142,14 +151,14 @@ def get_basic_number_image(nn: Sequence[int | FormatNum] | bytearray, digit_imag
 if __name__ == '__main__':
 	import sys
 	from pprint import pp
-	width = 24
-	bdiprm = BasicDigitImage.calc_scale_from_width(width)
-	bdi = BasicDigitImage(width, *bdiprm)
+	height = 40
+	bdiprm = BasicDigitImage.calc_scale_from_height(height)
+	bdi = BasicDigitImage(bdiprm)
 	di0 = bdi.get(0)
 	di0.show()
-	s_width = 16
-	s_param = BasicDigitImage.calc_scale_from_width(s_width)
-	digit_image_S = BasicDigitImage(s_width, *s_param)
+	s_height = 30
+	s_param = BasicDigitImage.calc_scale_from_height(s_height)
+	digit_image_S = BasicDigitImage(s_param)
 	multi_image_numbers = [0, 2]
 	multi_number_image_size = (len(multi_image_numbers) * digit_image_S.size[0], digit_image_S.size[1])
 	multi_number_image = Image.new('L', multi_number_image_size, (0,))
@@ -160,9 +169,9 @@ if __name__ == '__main__':
 		multi_number_image.paste(digit_image, offset)
 		offset = offset[0] + x_offset, 0
 	# digit_strokes_L = BasicDigitStrokes(scale=20, offset=(8, 8))
-	l_width = 32
-	l_param = BasicDigitImage.calc_scale_from_width(s_width)
-	digit_image_L = BasicDigitImage(l_width, *l_param)
+	l_height = 70
+	l_param = BasicDigitImage.calc_scale_from_height(s_height)
+	digit_image_L = BasicDigitImage(l_param)
 	digit_image_L_0 = digit_image_L.get(0)
 	digit_image_L_0.show()
 	scale = 16
