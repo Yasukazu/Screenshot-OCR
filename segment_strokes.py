f67f6aed571660ed9644ae0bb752c2c923cce93f
Yasukazu akino <yasukazu@makyno.jp>
2025-03-14 20:40:51 +0900

    SegmentStrokes.get needs binary

--- segment_strokes.py
+++ segment_strokes.py
@@ -1,18 +1,20 @@
 from typing import Callable, Sequence
 from functools import lru_cache
 from strok7 import SpPair
-from seg_7_digits import SEG_POINT_PAIR_DIGIT_ARRAY, Seg7, expand_to_sp_pairs, hex_to_seg7, expand_to_xy_list_list
+from seg_7_digits import SEG_POINT_PAIR_DIGIT_ARRAY, Seg7, expand_to_sp_pairs, hex_to_seg7, expand_to_xy_list_list, bin_to_seg7
+
 import numpy as np
-STROKE_SIZE = 17
+STROKE_SIZE = 18
 class SegmentStrokes:
-	f'''strokes[{STROKE_SIZE}]'''
+	f'''strokes[{STROKE_SIZE}]: Use hex_to_seg7 to get from digit'''
+
 	def __init__(self, scale: int=1, offset: tuple[int, int]=(0, 0)): # slant: StrokeSlant=StrokeSlant.SLANT00, 
 		# self.slant = slant
 		self.scale = scale
 		self.offset = offset
 		self.offset_ndarray: np.ndarray | None = None
 		self.size = scale, 2 * scale
-		self.get: Callable[[int], np.ndarray]= lru_cache(maxsize=STROKE_SIZE)(self._scale_offset_array)
+		self.get: Callable[[int], np.ndarray]= lru_cache(maxsize=STROKE_SIZE)(self._scale_offset_bin)
 
 	@classmethod
 	def get_max(cls)-> int:
@@ -24,6 +26,13 @@ class SegmentStrokes:
 	
 	def _scale_offset_array(self, n: int)-> np.ndarray:
 		array = self.expand_int_to_xy_array(n)
+		if not self.offset_ndarray:
+			self.offset_ndarray = np.array(self.offset, dtype=np.int16)
+		return array * self.scale + self.offset_ndarray
+
+	def _scale_offset_bin(self, b: int)-> np.ndarray:
+		seg7 = bin_to_seg7(b) # if raw else hex_to_seg7(b)
+		array = self.expand_seg7_to_xy_array(seg7)
 		if not self.offset_ndarray:
 			self.offset_ndarray = np.array(self.offset, dtype=np.int16)
 		return array * self.scale + self.offset_ndarray
@@ -62,13 +71,18 @@ if __name__ == '__main__':
 	scale = 8
 	offset = (3, 5)
 	ss = SegmentStrokes(scale=scale, offset=offset)
-	ss_0 = ss.get(0)
-	pp(ss_0)
+	abcd_seg = Seg7.A | Seg7.B | Seg7.C | Seg7.D
+	abcd_bin = abcd_seg.value
+	n = 0
+	n_seg7 = hex_to_seg7(n)
+	n_bin = n_seg7.value
+	abcd_strokes = ss.get(n_bin)
+	pp(abcd_strokes)
 	img_size = (np.array([scale, 2 * scale], dtype=np.int16) + 2 * np.array(offset, dtype=np.int16)).tolist()
 	from PIL import Image, ImageDraw
-	image = Image.new('L', img_size)
+	image = Image.new('L', img_size, 0x1f)
 	draw = ImageDraw.Draw(image)
-	for stroke in ss_0:
+	for stroke in abcd_strokes:
 		ss = stroke.tolist()
 		draw.line(ss[0] + ss[1], width=1, fill=255)
 	image.show()
