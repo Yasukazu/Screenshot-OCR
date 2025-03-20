b5292c46763ffccf386cc941a44a71031a0419a1
Makino,Yasukazu <mkn@yskz.dev>
2025-03-20 09:08:35 +0900

    expand_y

--- seg7yx.py
+++ seg7yx.py
@@ -19,7 +19,14 @@ class Seg7yx:
 		for y, xx in enumerate(self.yx):
 			r += [(x, y) for x in xx]
 		return tuple(r)
-	def slanted(self, slant_r: float):
+	
+	def expand_y(self):
+		r = []
+		for i, xx in enumerate(self.yx):
+			r += [(x, i) for x in xx]
+		return tuple(r)
+
+	def slanted_yx(self, slant_r: float)-> Sequence[Sequence[float]]:
 		return tuple([self.slanted_xx(slant_r, xx, y) for y, xx in enumerate(self.yx)])
 	@classmethod
 	def slanted_new(cls, slant_r: float, yx: 'Seg7yx')-> 'Seg7yx':
@@ -37,11 +44,20 @@ class Seg7yx:
 from enum import Enum
 class Seg7yxSlant(Enum):
 	SLANT00 = Seg7yx().yx # ((0, 1), (0, 1), (0, 1))
-	SLANT02 = Seg7yx().slanted(0.2) # ((0.2, 1.2), (0.1, 1.1), (0.0, 1.0))
-
+	SLANT02 = Seg7yx().slanted_yx(0.2) # ((0.2, 1.2), (0.1, 1.1), (0.0, 1.0))
+class SlantedSeg7yx(Enum):
+	SLANT00 = Seg7yx() # ((0, 1), (0, 1), (0, 1))
+	SLANT02 = Seg7yx().slanted_new(0.2, SLANT00) # ((0.2, 1.2), (0.1, 1.1), (0.0, 1.0))
 if __name__ == '__main__':
 	from pprint import pp
+	sl0 = SlantedSeg7yx.SLANT00
+	sl2 = SlantedSeg7yx.SLANT02
+	seg_points = sl2.value.expand_y()
+	import numpy as np
+	offset = np.array([55, 77])
+	scale_offset_seg_points = 7 * np.array(seg_points) + offset
 	s7yx = Seg7yx()
 	pp(s7yx.to_list())
-	s7yx2 = s7yx.slanted(0.2)
+	s7yxn = Seg7yx.slanted_new(0.2, s7yx)
+	s7yx2 = s7yx.slanted_yx(0.2)
 	pp(s7yx2)
