d087a50de1230ef9e54d6b8c05c8a4d657c9599c
Yasukazu akino <yasukazu@makyno.jp>
2025-03-21 22:24:32 +0900

    Seg7yx.offset_scaled

--- seg7yx.py
+++ seg7yx.py
@@ -8,14 +8,14 @@ class Seg7yx:
 		5: (0, 1), 2: (1, 1),
 		4: (0, 2), 3: (1, 2)
 	}
-	def __init__(self, yx: Sequence[Sequence[float]]= (
-			(0, 1),
-			(0, 1),
-			(0, 1),
-		), scale=1):
+	def __init__(self, yx: Sequence[Sequence[float]]= [
+			[0, 1],
+			[0, 1],
+			[0, 1],
+		]): #scale=1):
 		self.yx = yx
 		# self.node_list = [scale * Seg7yx.node_dict[x] for x in range(6)]
-		self.scale = scale
+		# self.scale = scale
 
 	def to_list(self)-> list[list[float]]:
 		r = []
@@ -44,14 +44,38 @@ class Seg7yx:
 		for y, xx in enumerate(self.yx):
 			r += [(x, y) for x in xx]
 		return tuple(r)
+	def scale_offset(self, scale=1, offset=(0, 0)):
+		self._scale(scale)
+		self._offset(offset)
+	def slant_scale_offset(self, slant=0.0, scale=1, offset=(0, 0)):
+		self._slant(slant)
+		self._scale(scale)
+		self._offset(offset)
+	def _offset(self, offset=(0, 0)):
+		self.yx = [[xx[0] + offset[0], xx[1] + offset[1]] for xx in (self.yx)]
+	def _scale(self, scale=1):
+		self.yx = [[xx[0] * scale, xx[1] * scale] for xx in (self.yx)]
+	def _slant(self, slant_r: float):
+		self.yx = [self.slanted_xx(slant_r, xx, y) for y, xx in enumerate(self.yx)]
 	def slanted(self, slant_r: float):
-		return tuple([self.slanted_xx(slant_r, xx, y) for y, xx in enumerate(self.yx)])
+		return [self.slanted_xx(slant_r, xx, y) for y, xx in enumerate(self.yx)]
+
+	@classmethod
+	def offset_scaled(cls, offset: Sequence[int], scale: int, xy: Sequence[list[float]]):
+		return cls._offset(offset, cls._scale(scale, xy))
+	@classmethod	
+	def _offset(cls, offset: Sequence[int], xy: Sequence[Sequence[float]]):
+		return [[x + offset[0], y + offset[1]] for (x, y) in xy]
+	@classmethod
+	def _scale(cls, scale: int, xy: Sequence[list[float]]):
+		return [[xx[0] * scale, xx[1] * scale] for xx in xy]
+
 	@classmethod
 	def slanted_new(cls, slant_r: float, yx: 'Seg7yx')-> 'Seg7yx':
-		return cls(tuple([cls.slanted_xx(slant_r, xx, y) for y, xx in enumerate(yx.yx)]))
+		return cls([cls.slanted_xx(slant_r, xx, y) for y, xx in enumerate(yx.yx)])
 	@classmethod
 	def slanted_xx(cls, slant: float, xx: Sequence[float], y: int)-> Sequence[float]:
-		return tuple([cls.slanted_x(x, y, slant) for x in xx])
+		return [cls.slanted_x(x, y, slant) for x in xx]
 	@classmethod
 	def slanted_x(cls, x: float, y: int, slant: float)-> float:
 		return x + slant * cls.slant_ratio_by_y(y)
@@ -66,9 +90,14 @@ class Seg7yxSlant(Enum):
 
 if __name__ == '__main__':
 	from pprint import pp
-	pp(Seg7yxSlant.SLANT00)
-	pp(Seg7yxSlant.SLANT02)
+
 	s7yx = Seg7yx()
+	offset_scaled = Seg7yx.offset_scaled((100,200), 10, s7yx.slanted_seg7(0.2))
+	s7yx.slant_scale_offset(0.2, 10, (100, 200))
+	pp(s7yx.yx)
+
 	pp(s7yx.to_list())
 	s7yx2 = s7yx.slanted(0.2)
 	pp(s7yx2)
+	pp(Seg7yxSlant.SLANT00)
+	pp(Seg7yxSlant.SLANT02)
