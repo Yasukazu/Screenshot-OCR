6a44ffed2cc1067d42803afef2532525315c15af
Yasukazu akino <yasukazu@makyno.jp>
2025-03-27 20:05:54 +0900

    draw_boxes

--- tool_pyocr.py
+++ tool_pyocr.py
@@ -3,7 +3,7 @@ from pprint import pp
 from pathlib import Path
 import os
 from dotenv import load_dotenv
-from PIL import Image
+from PIL import Image, ImageDraw
 import pyocr
 import pyocr.builders
 #import cv2
@@ -49,26 +49,42 @@ class MyOcr:
 
 	def __init__(self):
 		self.path_feeder = PathFeeder(input_dir=MyOcr.input_dir, type_dir=False)
+		self.txt_lines: Sequence[pyocr.builders.LineBox] | None = None
+		self.image: Image.Image | None = None
 	def each_path_set(self):
 		for stem in self.path_feeder.feed(delim=self.delim, padding=False):
 			yield PathSet(self.path_feeder.dir, stem, self.path_feeder.ext)
 	def run_ocr(self, path_set: PathSet, lang='jpn+eng'):
 		#stem_without_delim = ''.join([s for s in path_set[1] if s!= self.delim])
 		fullpath = path_set.path / (path_set.stem_without_delim(self.delim) + path_set.ext)
-		img1 = Image.open(fullpath).convert('L')
-		return self.tool.image_to_string(
-			img1,
+		self.image = Image.open(fullpath).convert('L')
+		self.txt_lines = self.tool.image_to_string(
+			self.image,
 			lang=lang,
 			builder=pyocr.builders.LineBoxBuilder(tesseract_layout=3) # Digit
 		)
+		return self
+	
+	def draw_boxes(self):
+		if not self.txt_lines:
+			raise ValueError('txt_lines is None!')
+		if not self.image:
+			raise ValueError('Image is None!')
+		draw = ImageDraw.Draw(self.image)
+		for line in self.txt_lines:
+			cood = line.position[0] + line.position[1]
+			draw.rectangle(cood, outline=0x55)
+		self.image.show()
+		return self
 
-
-	@classmethod
-	def check_date(cls, path_set: PathSet, txt_lines: Sequence[pyocr.builders.LineBox]):
-		n_gyoumu = next_gyoumu(txt_lines)
+	#@classmethod
+	def check_date(self, path_set: PathSet):#, txt_lines: Sequence[pyocr.builders.LineBox]):
+		if not self.txt_lines:
+			raise ValueError('txt_lines is None!')
+		n_gyoumu = next_gyoumu(self.txt_lines)
 		gyoumu_date = get_date(n_gyoumu)
 		if gyoumu_date != (int(path_set.stem.split()[0]), int(path_set.stem.split()[1])): # path_feeder.month, 
-			ValueError(f"Unmatch {gyoumu_date} : {path_set.stem}!")
+			raise ValueError(f"Unmatch {gyoumu_date} : {path_set.stem}!")
 
 '''for w_box in n_gyoumu.word_boxes:
 	pp(w_box.content)
@@ -78,8 +94,7 @@ for tx in txt_lines:
 def main():
 	my_ocr = MyOcr()
 	for path_set in my_ocr.each_path_set():
-		result = my_ocr.run_ocr(path_set=path_set)
-		my_ocr.check_date(path_set, result)
+		my_ocr.run_ocr(path_set=path_set).draw_boxes().check_date(path_set)
 		pp(path_set)
 
 
