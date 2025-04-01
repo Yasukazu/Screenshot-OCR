3be9d8234644d5e32891ac4662c9d3774eaaf55d
Yasukazu akino <yasukazu@makyno.jp>
2025-04-01 15:05:47 +0900

    stems

--- convert_to_pdf.py
+++ convert_to_pdf.py
@@ -14,9 +14,11 @@ class PdfLayout(Enum):
 	a3lp = (img2pdf.mm_to_pt(420),img2pdf.mm_to_pt(297))
 
 
-def convert_to_pdf(fullpath: Path, names: Sequence[str], layout=PdfLayout.a4pt):
+def convert_to_pdf(output_fullpath: Path, stems: Sequence[str], layout=PdfLayout.a4pt):
 	layout_fun = img2pdf.get_layout_fun(layout.value)
-	with fullpath.open("wb") as f:
+	parent = output_fullpath.parent
+	names = [str(parent / stem)+'.png' for stem in stems]
+	with output_fullpath.open("wb") as f:
 		buff = img2pdf.convert(layout_fun=layout_fun, *names, rotation=img2pdf.Rotation.ifvalid)
 		if buff:
 			f.write(buff)
@@ -24,8 +26,17 @@ def convert_to_pdf(fullpath: Path, names: Sequence[str], layout=PdfLayout.a4pt):
 if __name__ == '__main__':
 	from dotenv import load_dotenv
 	load_dotenv()
-	import os
+	import os, sys
+	block = int(sys.argv[1])
 	input_dir = Path(os.environ['SCREEN_BASE_DIR']) / '2025' / '03' 
-	fullpath = input_dir/ 't._202503_png.pdf'
-	names = [str(n) for n in input_dir.glob("t._202503*.png")]
-	convert_to_pdf(fullpath=fullpath, names=names)
+	fullpath = input_dir/ '202503_T_images-1.pdf'
+	from tool_pyocr import Main
+	main = Main(month=3, app=1)
+	cur = main.con.cursor()
+	result = cur.execute(f"SELECT `day`, `stem` from `{main.tbl_name}` ORDER BY `day`;")
+	frm = to = 0
+	if block == 2:
+		frm = 10
+		to = 21
+	names = [it[1] for it in result][frm: to] # if it[0] <= 10]
+	convert_to_pdf(output_fullpath=fullpath, stems=names)
