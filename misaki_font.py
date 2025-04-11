1ac65648bef5352494a389d2dbad0d40b4ee4571
Makino,Yasukazu <mkn@yskz.dev>
2025-04-11 18:55:54 +0900

    hex comma

--- misaki_font.py
+++ misaki_font.py
@@ -12,11 +12,15 @@ font_dir = Path(screen_base_dir_name) / 'font'
 if not font_dir.exists():
 	raise ValueError(f"`{font_dir=}` does not exists!")
 arc_font_file_name = 'misaki_gothic-digit.tif'
-FONT_SIZE = (8, 10)
+FONT_SIZE = (8, 8)
+type ku = tuple[tuple[int, int], int]
+DIGIT_KU: ku = (3, 16),10
+HEX_KU: ku = (3, 33),6
+TEN_KU: ku = (1, 4),2
 def array(seq):
-	return np.array(seq, np.int)
+	return np.array(seq, np.int64)
 def get_misaki_digit_images(scale=1):
-	re_size = (np.array(FONT_SIZE, dtype=np.int64) * scale).tolist() if scale > 1 else None
+	re_size = (array(FONT_SIZE) * scale).tolist() if scale > 1 else None
 	arc_font_fullpath = font_dir / arc_font_file_name
 	image_list = {}
 	if arc_font_fullpath.exists():
@@ -31,21 +35,25 @@ def get_misaki_digit_images(scale=1):
 	if not font_fullpath.exists():
 		raise ValueError(f"`{font_fullpath=}` does not exists!")
 	full_image = Image.open(str(font_fullpath))
-	num_ku_ten = (3, 16)
 	def kuten_to_xy(*kuten: int):
 		return kuten[1] - 1, kuten[0] - 1
-	num_pos = kuten_to_xy(3, 16)
-	num_addr = np.array(num_pos, dtype=np.int64)
-	offset = (num_addr * 8) - np.array([0, 2], dtype=np.int64)
-	end_point = offset + np.array(FONT_SIZE)
-	area = np.array([offset, end_point])
 	image_list = []
-	for i in range(10):
-		img_area = area.ravel().tolist()
-		font_part = full_image.crop(img_area)
-		font_part = font_part.resize([32, 40])
-		area += np.array([8, 0])
-		image_list.append(font_part)
+	def append_fonts(ku: ku): 
+		num_ku_ten, num_range = ku
+		num_pos = kuten_to_xy(*num_ku_ten)
+		num_addr = array(num_pos)
+		offset = (num_addr * 8) # - array([0, 2])
+		end_point = offset + np.array(FONT_SIZE)
+		area = array([offset, end_point])
+		for i in range(num_range):
+			img_area = area.ravel().tolist()
+			font_part = full_image.crop(img_area)
+			#font_part = font_part.resize([32, 40])
+			area += np.array([8, 0])
+			image_list.append(font_part)
+	append_fonts(DIGIT_KU)
+	append_fonts(HEX_KU)
+	append_fonts(TEN_KU)
 	image_list[0].save(str(arc_font_fullpath),
 		save_all=True, append_images=image_list[1:]) # compression='tiff_lzw'
 	return {n: img for (n, img) in enumerate(image_list)}
