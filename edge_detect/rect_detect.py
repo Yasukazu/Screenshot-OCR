ab2c4a6cd8cfec64e43ca05f185136cdb990af6b
Makino,Yasukazu <mkn@yskz.dev>
2025-09-25 23:52:14 +0900

    cvtColor B, no binarize

--- edge_detect/rect_detect.py
+++ edge_detect/rect_detect.py
@@ -1,40 +1,72 @@
 from math import sqrt, floor, dist
 from typing import Sequence
-from sys import argv, exit
+from sys import argv, exit, stdout
 import cv2 
 import numpy as np 
 from numpy import ndarray
+from logging import getLogger, INFO, StreamHandler
+logger = getLogger(__name__)
+logger.setLevel(INFO)
+stdout_handler = StreamHandler(stdout)
+stdout_handler.setLevel(INFO)
+logger.addHandler(stdout_handler)
 
-def main(filename: str, cutoff=0.3):
+def main(filename: str, cutoff=0.3, BGR='B'):
+    logger.info("main started.")
     # Load the image 
     image = cv2.imread(filename) # 'path/to/your/image.jpg') 
     image_h, image_w, _ = image.shape 
     image_min = min([image_h, image_w])
+    src_f = image.astype(np.float64)
     # Convert to grayscale 
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) 
+    luminosity_result = np.zeros((image_h, image_w), np.uint8)
+    # ルミナンス法（Luminosity Method）
+    bgr_pos = 'BGR'.index(BGR)
+    for i in range(image_h):
+        for j in range(image_w):
+            #blue = src_f[i, j, 0]
+            #green = src_f[i, j, 1]
+            #red = src_f[i, j, 2]
+            luminosity_result[i, j] = src_f[i, j, bgr_pos] # 0.02126*red + 0.5152*green + 0.722*blue
+    # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) 
     
     # Apply Gaussian blur to reduce noise and improve edge detection 
-    blurred = cv2.GaussianBlur(gray, (5, 5), 0) 
+    blurred = cv2.GaussianBlur(luminosity_result, (57, 57), 3) # medianBlur
+
+    # Binarize
+    #thresh_binarized = cv2.threshold(blurred, 200, 255,cv2.THRESH_BINARY)
+    #thresh_binarized = cv2.adaptiveThreshold(blurred, 255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\ cv2.THRESH_BINARY,11,2)
     
     # Perform edge detection 
     edges = cv2.Canny(blurred, 50, 150) 
     
     # Find contours in the edged image 
     contours, _ = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) 
-    
-    image_cutoff = floor(image_min * cutoff) # / 16) or 4
+    if not len(contours):
+        raise ValueError("No contours detected!")
+    logger.info("%s contours detected.", len(contours))
+    #limit = 4 # len(contours) // 32
+    #limited_cont = []
+    sorted_contours = sorted(contours, key=cv2.contourArea, reverse=True) #[:limit] # (limit // 2):limit] 
+    # if len(ct) == 4: limited_cont.append(ct) if len(limited_cont) >= limit: break
+    # logger.info("%s contours limited.", len(limited_cont))
+    # image_cutoff = floor(image_min * cutoff) # / 16) or 4
     # Loop over the contours 
-    for contour in contours: 
+    limit = 16
+    added = 0
+    for contour in sorted_contours: 
         # Approximate the contour to a polygon 
         epsilon = 0.02 * cv2.arcLength(contour, True) 
         approx_cont = cv2.approxPolyDP(contour, epsilon, True) 
-        
         # Check if the approximated contour has 4 points (rectangle) 
         if len(approx_cont) == 4:
-            min_dist = get_min_distance(approx_cont)
-            if min_dist > image_cutoff: 
+            # min_dist = get_min_distance(approx_cont)
+            # if min_dist > image_cutoff: 
                 # Draw the rectangle on the original image 
-                cv2.drawContours(image, [approx_cont], -1, (0, 255, 0), 2) 
+            cv2.drawContours(image, [approx_cont], -1, (0xff, 0, 0), 2) 
+            added += 1
+            if added >= limit:
+                break
     
     # Display the result 
     cv2.imshow('Detected Rectangles', image) 
