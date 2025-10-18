36f24e998934229340114c181a7681663646dd11
Makino,Yasukazu <mkn.git@yasukazu.jp>
2025-10-18 17:36:46 +0000

    Detect horizontal lines in `taimee-test.png` using np.unique() is 1 and np.any()

--- edge_detect/matblotlib_draw.py
+++ edge_detect/matblotlib_draw.py
@@ -1,9 +1,37 @@
 import cv2
 import matplotlib.pyplot as plt
-
+import numpy as np
 # 画像を読み込む
-image = cv2.imread('data/sample.png')
+image_path = 'data/taimee-test.png'
+image = cv2.imread(image_path)
+if image is None:
+    raise ValueError("Error: Could not load image: %s" % image_path)
 
-# BGRからRGBに変換して表示
+# 2. Get image dimensions
+height, width = image.shape[:2]
+
+# 3. Define the y-coordinate for the horizontal line
+# Here, we get the horizontal line at the vertical center of the image.
+for y_coord in range(height):
+    h_line = image[y_coord, :]
+    # h_line_is_non_white = (h_line < 0xff).all() & (h_line > 0).all()
+    if len(np.unique(h_line)) == 1 and bool((h_line < 255).any()):
+        print("%d" % y_coord)
+        cv2.line(image, (0, y_coord), (width // 16, y_coord), (255,0,0))
 plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 plt.show()
+
+# Result
+'''102
+103
+105
+106
+107
+108
+109
+334
+335
+603
+604
+829
+830'''
