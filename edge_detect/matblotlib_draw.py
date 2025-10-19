b7c232e609ddffdb5d25f3cd153019db369d8422
Makino,Yasukazu <mkn.git@yasukazu.jp>
2025-10-19 18:20:15 +0000

    cut heading area

--- edge_detect/matblotlib_draw.py
+++ edge_detect/matblotlib_draw.py
@@ -1,26 +1,33 @@
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
-# 画像を読み込む
+
 image_path = 'data/taimee-test.png'
 image = cv2.imread(image_path)
 if image is None:
     raise ValueError("Error: Could not load image: %s" % image_path)
 
-# 2. Get image dimensions
 height, width = image.shape[:2]
-
-# 3. Define the y-coordinate for the horizontal line
-# Here, we get the horizontal line at the vertical center of the image.
+y_coords = []
 for y_coord in range(height):
     h_line = image[y_coord, :]
     # h_line_is_non_white = (h_line < 0xff).all() & (h_line > 0).all()
     if len(np.unique(h_line)) == 1 and bool((h_line < 255).any()):
-        print("%d" % y_coord)
+        y_coords.append(y_coord)
+        # print("%d" % y_coord)
         cv2.line(image, (0, y_coord), (width // 16, y_coord), (255,0,0))
-plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-plt.show()
+    
+# cut heading area
+last = y_coords[0]
+for y_coord in y_coords[1:]:
+    if y_coord > last + 2:
+        break
+    else:
+        last = y_coord
 
+plt.imshow(cv2.cvtColor(image[last + 1:, :], cv2.COLOR_BGR2RGB))
+plt.show()
+print(y_coords[last + 1:])
 # Result
 '''102
 103
