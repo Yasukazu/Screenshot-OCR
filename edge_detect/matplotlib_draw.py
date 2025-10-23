c475606876b55fb27b8e66e5a7497d86b0d55c25
Makino,Yasukazu <mkn@yskz.dev>
2025-10-23 15:52:18 +0900

    scan left-top area

--- edge_detect/matplotlib_draw.py
+++ edge_detect/matplotlib_draw.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 image_path = 'DATA/taimee-test.png'
-image = cv2.imread(image_path)
+image = cv2.cvtColor(cv2.imread(image_path), cv2.COLOR_BGR2GRAY)
 if image is None:
     raise ValueError("Error: Could not load image: %s" % image_path)
 
@@ -15,17 +15,40 @@ for y_coord in range(height):
     if len(np.unique(h_line)) == 1 and bool((h_line < 255).any()):
         y_coords.append(y_coord)
         # print("%d" % y_coord)
-        cv2.line(image, (0, y_coord), (width // 16, y_coord), (255,0,0))
+        cv2.line(image, (0, y_coord), (width // 16, y_coord), 255) #,0,0))
     
 # cut heading area
 last = y_coords[0]
-for y_coord in y_coords[1:]:
+ln = -1
+for ln, y_coord in enumerate(y_coords[1:]):
     if y_coord > last + 2:
         break
     else:
         last = y_coord
+cut_image = image[last + 1:, :]
 
-plt.imshow(cv2.cvtColor(image[last + 1:, :], cv2.COLOR_BGR2RGB))
+# mask image of a left-top circle as blank
+## find the top block
+cut_height = y_coords[ln + 1] - last
+assert cut_height > 0
+### scan left-top area for a (non-white) shape
+for x, x_cd in enumerate(range(width)):
+    v_line = cut_image[:(cut_height - 1), x_cd]
+    if len(np.unique(v_line)) > 1:
+        break
+for x_cd in range(width - x):
+    v_line = cut_image[:(cut_height - 1), x_cd + x]
+    if len(np.unique(v_line)) == 1 and (v_line == 255).all():
+        break
+cut_x = x_cd + x
+mask = np.full((cut_height, width), 255, np.uint8)
+# cv2.circle(mask, (width // 16, height // 16), width // 16, (255, 255, 255), -1)
+#cut_image = cv2.bitwise_and(cut_image, cut_image, mask=mask)
+# plt.imshow(cv2.cvtColor(cut_image, cv2.COLOR_BGR2RGB))
+fig, ax = plt.subplots()
+ax.invert_yaxis()
+ax.xaxis.tick_top()
+ax.imshow(cv2.cvtColor(cut_image, cv2.COLOR_BGR2RGB))
 plt.show()
 print(y_coords[last + 1:])
 # Result
@@ -41,4 +64,6 @@ print(y_coords[last + 1:])
 603
 604
 829
-830'''
+830
+
+x,x_cd=(28,168)'''
