a76051c0b16f9e7e183124a1dc1a26ca16970b66
Makino,Yasukazu <mkn.git@yasukazu.jp>
2025-10-25 12:38:45 +0000

    image_filter module

---
+++ edge_detect/image_filter.py
@@ -0,0 +1,53 @@
+from cv2 import UMat
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+from pathlib import Path
+
+
+def taimee(image: UMat | Path | str) -> UMat:
+    image_fullpath = None
+    match(image):
+        case UMat():
+            pass
+        case Path():
+            image_fullpath = image.resolve()
+        case str():
+            image_fullpath = Path(image).resolve()
+    if image_fullpath:
+        image = cv2.imread(image_fullpath)
+    height, width = image.shape[:2]
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    h_lines = []
+    for y_coord in range(height):
+        h_line = image[y_coord, :]
+        if len(np.unique(h_line)) == 1 and bool((h_line < 255).any()):
+            h_lines.append(y_coord)
+    if len(h_lines) < 2:
+        raise ValueError("Not enough heading line found!")
+    # cut heading area
+    last = h_lines[0]
+    for ln, y_coord in enumerate(h_lines[1:]):
+        if y_coord > last + 2:
+            break
+        else:
+            last = y_coord
+    image = image[last + 1:, :]
+
+    # mask image of a left-top circle as blank
+    ## find the top block
+    cut_height = h_lines[ln + 1] - last
+    assert cut_height > 0
+    ### scan left-top area for a (non-white) shape
+    for x, x_cd in enumerate(range(width)):
+        v_line = image[:(cut_height - 1), x_cd]
+        if len(np.unique(v_line)) > 1:
+            break
+    for x_cd in range(width - x):
+        v_line = image[:(cut_height - 1), x_cd + x]
+        if len(np.unique(v_line)) == 1 and (v_line == 255).all():
+            break
+    cut_x = x_cd + x
+    # draw a white rectangle
+    cv2.rectangle(image, (0, 0), (cut_x, cut_height), (255, 255, 255), -1)
+    return image
--- edge_detect/matplotlib_draw.py
+++ edge_detect/matplotlib_draw.py
@@ -3,22 +3,23 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 from pathlib import Path
-from dotenv import load_dotenv
-env_path = Path('edge_detect/.env')
+# from dotenv import load_dotenv
+import tomllib
+config_file = Path('.env')
 #env_path = Path(__file__).resolve().parent / '.env'
-if not env_path.exists():
-    raise ValueError("Error: .env file not found: %s" % env_path)
-with env_path.open() as f:
-    load_dotenv(override=True, stream=f)
-
+if not config_file.exists():
+    raise ValueError("Error: .env file not found: %s" % config_file)
+# with config_file.open('r') as f: load_dotenv(override=True, stream=f)
+with config_file.open('rb') as f:
+    config = tomllib.load(f)
 # parent_dir = Path(__file__).resolve().parent.parent
-image_dir = Path(os.environ['IMAGE_DIR']) # parent_dir / 'DATA'
+image_dir = Path(config['IMAGE_DIR']) # Path(os.environ['IMAGE_DIR']) # parent_dir / 'DATA'
 if not image_dir.exists():
     raise ValueError("Error: image dir not found: %s" % image_dir)
-filename = os.environ['IMAGE_FILENAME']
+filename = config['IMAGE_FILENAME']
 image_path = Path(image_dir) / filename
 if not image_path.exists():
-    raise ValueError("Error:    image file not found: %s" % image_path)
+    raise ValueError("Error: image file not found: %s" % image_path)
 image_fullpath = image_path.resolve()
 image = cv2.cvtColor(cv2.imread(image_fullpath), cv2.COLOR_BGR2GRAY)
 if image is None:
@@ -69,7 +70,7 @@ ax.invert_yaxis()
 ax.xaxis.tick_top()
 ax.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 plt.show()
-input('Press Enter to exit:')
+print(cut_x, cut_height)
 # print(y_coords[last + 1:])
 # Result
 '''102
