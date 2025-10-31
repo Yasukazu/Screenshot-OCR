8cbd2cb42ca4255d1745c793e809c44a5d9cd8ff
Makino,Yasukazu <mkn@yskz.dev>
2025-10-31 14:04:58 +0900

    2-way config: env_file and toml_file

--- edge_detect/matplotlib_draw.py
+++ edge_detect/matplotlib_draw.py
@@ -3,20 +3,23 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 from pathlib import Path
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 import tomllib
-config_file = Path('.env')
+env_file = Path('.env')
 #env_path = Path(__file__).resolve().parent / '.env'
-if not config_file.exists():
-    raise ValueError("Error: .env file not found: %s" % config_file)
-# with config_file.open('r') as f: load_dotenv(override=True, stream=f)
+if not env_file.exists():
+    raise ValueError("Error: .env file not found: %s" % env_file)
+with env_file.open('r') as f:
+    load_dotenv(override=True, stream=f)
+config_file = Path(os.environ['FILTER_TOML_PATH'])
 with config_file.open('rb') as f:
     config = tomllib.load(f)
 # parent_dir = Path(__file__).resolve().parent.parent
-image_dir = Path(config['IMAGE_DIR']) # Path(os.environ['IMAGE_DIR']) # parent_dir / 'DATA'
+image_path_config = config['image-path']
+image_dir = Path(image_path_config['dir'])
 if not image_dir.exists():
     raise ValueError("Error: image dir not found: %s" % image_dir)
-filename = config['IMAGE_FILENAME']
+filename = image_path_config['filename']
 image_path = Path(image_dir) / filename
 if not image_path.exists():
     raise ValueError("Error: image file not found: %s" % image_path)
@@ -25,15 +28,15 @@ image = cv2.imread(image_fullpath) #cv2.cvtColor(, cv2.COLOR_BGR2GRAY)
 if image is None:
     raise ValueError("Error: Could not load image: %s" % image_fullpath)
 from image_filter import taimee
-
-ret_image = taimee(image)
+threshold, fixed_image = taimee(image)
 
 fig, ax = plt.subplots()
 ax.invert_yaxis()
 ax.xaxis.tick_top()
-ax.imshow(cv2.cvtColor(ret_image[1], cv2.COLOR_BGR2RGB))
+ax.imshow(fixed_image)
 plt.show()
-print(cut_x, cut_height)
+print(threshold)
+# print(cut_x, cut_height)
 # print(y_coords[last + 1:])
 # Result
 '''102
