d2aff634ad6aba52f227c348ffe2b17ae1b8b116
Makino,Yasukazu <mkn@yskz.dev>
2025-11-08 03:03:08 +0900

    concat any count

--- concat_images/concat_12.py
+++ concat_images/concat_12.py
@@ -1,15 +1,17 @@
 from pathlib import Path
 from cv2 import imread, hconcat, vconcat, imwrite
 
-img_dir = Path("~/Documents/screen/bank/").expanduser()
-img_list = list(img_dir.glob("*.jpg"))
-if len(img_list) != 12:
-	raise ValueError(f"Error: {len(img_list)} images found in '{img_dir}', expected 12.")
+img_dir = Path("~/Documents/kakeibo/202510/").expanduser()
+img_ext = ".png"
+img_list = list(img_dir.glob(f"*{img_ext}"))
+# if len(img_list) != 12: raise ValueError(f"Error: {len(img_list)} images found in '{img_dir}', expected 12.")
 img_list.sort()
 h_list = []
-for n, img_path in enumerate(img_list[:12]):
+max_item_count = 2
+item_count = -1
+for item_count, img_path in enumerate(img_list[:max_item_count]):
 	img = imread(str(img_path))
-	if not img or img.shape[0] == 0 or img.shape[1] == 0:
+	if img is None or img.shape[0] == 0 or img.shape[1] == 0:
 		raise ValueError(f"Error: {img_path} is not a valid image.")
 	# draw borders
 	for r in range(img.shape[2]):
@@ -18,11 +20,19 @@ for n, img_path in enumerate(img_list[:12]):
 		img[:, 0, r] = 0
 		img[:, -1, r] = 0
 	h_list.append(img)
-v_list = [hconcat(h_list[:6]), hconcat(h_list[6:])]
+assert item_count >= 0
+item_count += 1
+row_count = 1
+col_count = item_count // row_count
+assert col_count in [1, 2]
+v_list = [hconcat(h_list[:col_count])]
+if row_count >= 2:
+	v_list.append(hconcat(h_list[col_count:]))
 img = vconcat(v_list)
-output_path = img_dir / "concat-12.jpg"
+output_filename = f"concat-{row_count}-{col_count}" + img_ext
+output_path = img_dir / output_filename
 result = imwrite(str(output_path), img)
 if not result:
 	print(f"Error: failed to write '{output_path}'.")
 else:
-	print(f"Saved 12-concatenated image to '{output_path}'.")
+	print(f"Saved {row_count} rows and {col_count} columns concatenated image to '{output_path}'.")
