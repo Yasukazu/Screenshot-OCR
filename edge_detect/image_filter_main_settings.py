8b490ed3dffd96932d331205fed504a0216802e3
Makino,Yasukazu <mkn@yskz.dev>
2026-02-04 13:19:40 +0900

    TODO:default_factory:lambda

--- edge_detect/image_filter_main_settings.py
+++ edge_detect/image_filter_main_settings.py
@@ -9,24 +9,28 @@ def app_names():
 
 @dataclass
 class MainSettings:
+	from image_filter import APP_NAME
 	"""
 	Extract/OCR paystub text from an image file: Needs to specify the image file for OCR by files option (like '--files *.APP_NAME.png') or by shot_month option (like '--shot_month 1 2 3')
 	"""
 	# image_ext: str = help='Image file extension', default='.png')
 	# image_dir: str = help='Directory of image files', default='~/github/screen/DATA/')
-	app: str = ''
-	f"""Application name of the screenshot to execute OCR: choices={', '.join(app_names())}"""
+
+	app: str = 'nil' #: choices={', '.join(app_names())} 
+	"""Application name of the screenshot to execute OCR"""
+
 	app_name_to_stem_end: dict[str, str] = field(default_factory=lambda: {'taimee': '_jp.co.taimee', 'mercari': '_jp.mercari.work.android'})
-	"""Dictionary of app name to stem end"""
-	image_ext: list[str] = field(default_factory=lambda: [".png"])
-	"""Image file extension, starting with dot (e.g. ".png")"""
+	"""Dictionary of 'app name' to 'stem end': 'stem' means the part of the filename before the extension"""
+
+	image_ext_set: set[str] = field(default_factory=lambda:set("png"))
+	"""Image file extension set, every extention starts with dot (default is {'.png'})"""
 	image_dir: str = "~/Documents/screenshots"
 	"""Image file root directory"""
 	shot_month: list[int] = field(default_factory=list)
 	"""Choose Screenshot file by its month (MM part of [YYYY-MM-DD or YYYYMMDD]) included in filename stem. {Jan. is 01, Dec. is 12}(specified in a list like "[1,2,..]"""
-	files: list[str] = field(default_factory=list)
-	"""Image file fullpaths to commit OCR or to get parameters."""
-	app_stem_end: dict[str, str] = field(default_factory=lambda: {"taimee":"_jp.co.taimee", "mercari":"_jp.mercari.work.android"})
+	glob_pattern: str = "*.png"
+	"""Image file name pattern as glob pattern to commit OCR or to get parameters."""
+	app_name_to_suffix_set: dict[str, set[str]] = field(default_factory=lambda: {"taimee":{"co", "taimee"}, "mercari":{"mercari", "work"}})
 	"""Screenshot image file name endswith of the sclass APP_NAME(StrEnum): (specified in format as "<app_name1>:<stem_end1>,<stem_end2> ..." )"""
 	image_area_param_section_stem: str = "image_area_param"
 	app_border_ratio: dict[str, list[float]] = field( default_factory=lambda:{"taimee":[2.2,3.2]})
@@ -57,9 +61,14 @@ class MainSettings:
 	"""Screenshot image area parameter config file: format as INI or TOML(".ini" or ".toml" extention respectively): in [image_area_param.<app>] section, items as "<area_name>=[<p1>,<p2>,<p3>,<p4>]" (e.g. "heading=[0,106,196,-1]") """
 	ocr_filter_sqlite_db_name: str = "ocr-filter.db"
 	"""SQLite DB file is created under `image_dir`/{yyyy} directory(yyyy is like 2025)"""
-	data_year: int|None = None
-	"""Year for DB data (like 2025)"""
+	data_month: int = 0
+	"""Month of data (like -1, 0, 1, 2, ...). 0 means current month, negative value is difference from current month (like -1 means last month), positive value means month number (1: Jan, 2: Feb, ...);If this value is larger than current month, data's date is treated as the last year."""
 	show_ocr_area: bool = False
 	"""Show every area before commit OCR"""
-	exclude_area_param_set: list[str] = field(default_factory=list)
-	f"""Exclude a set of image area parameter names : { {f'{n}' for n in image_area_param_names()} }"""
+	exclude_area_param_set: list[str] = field(default_factory=list) # { {f'{n}' for n in image_area_param_names()} }
+	"""Exclude a set of image area parameter names"""
+
+if __name__ == '__main__':
+	from dataclass_binder import Binder
+	for line in Binder(MainSettings).format_toml_template():
+		print(line)
