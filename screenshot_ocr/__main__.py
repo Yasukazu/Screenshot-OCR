40f2f8c3e023539a44e7ad9def34e5ef056e7af9
Makino,Yasukazu <mkn@yskz.dev>
2026-04-11 23:04:17 +0900

    parse_toml Binder

---
+++ screenshot_ocr/__main__.py
@@ -0,0 +1,37 @@
+from sys import path as sys_path
+from pathlib import Path
+sys_path.insert(0, str(Path(__file__).parent))
+from edge_detect.image_filter_main_settings import print_toml_template
+from set_logger import set_logger
+logger = set_logger(__name__)
+if __name__ == '__main__':
+	from argparse import ArgumentParser
+	from sys import exit as sys_exit
+	from sys import argv
+	if len(argv) < 2:
+		argv += ['-h']
+	MAIN_SETTINGS_PATH_STR = "SCREENSHOT_OCR_MAIN_SETTINGS_PATH"
+	MAIN_SETTINGS_PATH_DEFAULT = "main-settings.toml"
+	parser = ArgumentParser(epilog="=== End of help ===", prog="screenshot-ocr", description=f"Screenshot OCR program: configuration file(in TOML format) fullpath is set by environment variable {MAIN_SETTINGS_PATH_STR}, or use default {MAIN_SETTINGS_PATH_DEFAULT} "	)
+	parser.add_argument('--print-template', action='store_true', help='print TOML template of MainSettings; remove leading "#" to specify any item')
+	parser.add_argument('--as-class', action='store_true', help='print TOML template of MainSettings as class')
+	args = parser.parse_args()
+	from edge_detect.image_filter_main_settings import MainSettings
+	if args.print_template:
+		print_toml_template(MainSettings, as_class=args.as_class)
+		sys_exit(0)
+	from os import environ
+	class EmptyPathError(Exception):
+		pass
+	try:
+		main_settings_path = environ[MAIN_SETTINGS_PATH_STR]
+		if not main_settings_path:
+			raise EmptyPathError()
+		logger.info("Using main settings path from environment variable %s as %s",MAIN_SETTINGS_PATH_STR, main_settings_path)
+	except (KeyError, EmptyPathError) as err:
+		main_settings_path = MAIN_SETTINGS_PATH_DEFAULT
+		logger.info("Using default main settings path %s since %s", main_settings_path, err)
+	from dataclass_binder import Binder
+	main_settings = Binder(MainSettings).parse_toml(main_settings_path)
+	logger.info("Main settings loaded: %s", main_settings)
+	
